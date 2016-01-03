ea8548dbcd87c4b09e502630f80c9d0f1af3e292
Nick Tikhonov <nt34@st-andrews.ac.uk>
2016-01-03 14:42:29 +0000

    Add fade-in animation to new tweets

--- client/portal/Tweets.js
+++ client/portal/Tweets.js
@@ -2,6 +2,17 @@ Template.Tweets.onCreated(function() {
   this.subscribe("tweets");
 });
 
+Template.Tweets.rendered = function() {
+  this.find('.animated')._uihooks = {
+    insertElement: function(node, next) {
+      $(node).addClass('off').insertBefore(next);
+      Tracker.afterFlush(function() {
+        $(node).removeClass('off');
+      });
+    }
+  }
+}
+
 Template.Tweets.helpers({
   'tweets': function() {
     return Tweets.find({}, {
